f55f465530f438f513f3208594420af43072a11b
Fer <Fer@DESKTOP-HKC2AKC>
2026-04-11 17:07:29 -0400

    Commit 2

--- tallerPOO2/src/tallerPOO2/App.java
+++ tallerPOO2/src/tallerPOO2/App.java
@@ -1,6 +1,8 @@
 package tallerPOO2;
 //Nombre: Fernando Javier Lincopan Araya 
 //Rut: 21.860.800-0
+import java.util.*;
+import java.util.*;
 public class App {
 
 	public static void main(String[] args) {
